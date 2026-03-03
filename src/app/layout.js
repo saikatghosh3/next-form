72e857ce106b2d600b9d2fdbfa92b1259d248ba5
saikatghosh-xs <saikat@roxnor.com>
2026-03-04 00:25:15 +0600

    font-updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" >
-      <body className={Inter.className}>
+      <body className={inter.className}>
         {children}
       </body>
     </html>
